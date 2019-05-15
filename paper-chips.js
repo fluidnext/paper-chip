ac0b0348f19dc9c49c50f26b3a6f18be064b4d25
BasToni <a.visalli@fluidnext.com>
2019-05-15 17:43:39 +0200

    Modify chips to accept array string

--- paper-chips.js
+++ paper-chips.js
@@ -2,40 +2,42 @@ import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import '@polymer/iron-icon';
 import '@polymer/paper-styles/default-theme';
+import '@collaborne/paper-chip/paper-chip';
+import '@collaborne/paper-chip/paper-chip-icons';
 
-import './paper-chip.js';
-import './paper-chip-icons.js';
 
 /**
-A list of chips that an be dynamically added/removed. This element can be used
-to let users enter a list of topics or names.
+ A list of chips that an be dynamically added/removed. This element can be used
+ to let users enter a list of topics or names.
 
-### Example
+ ### Example
 
-```html
-<paper-chips items=[[array]]></paper-chips>
-```
+ ```html
+ <paper-chips items=[[array]]></paper-chips>
+ ```
 
-### Items
+ ### Items
 
-The items given via the `items` property can have these properties:
+ The items given via the `items` property can have these properties:
 
-Property | Description
----------|-------------
-`id`     | Unique identification of the item (required)
-`name`   | Display name of the item
-`fixed`  | Boolean to indicate whether this item can be removed (`fixed` is `false`) or not (`fixed` is `true`)
+ Property | Description
+ ---------|-------------
+ `id`     | Unique identification of the item (required)
+ `name`   | Display name of the item
+ `fixed`  | Boolean to indicate whether this item can be removed (`fixed` is `false`) or not (`fixed` is `true`)
 
-@demo demo/chips.html
-*/
+ @demo demo/chips.html
+ */
 class PaperChips extends GestureEventListeners(PolymerElement) {
-	static get template() {
-		return html`
+    static get template() {
+        return html`
 		<style>
 			.chip {
 				@apply --paper-chips-chip;
 				margin-right: 5px;
 				padding-right: 6px;
+			    margin-left: 5px;
+				padding-left: 6px;
 				vertical-align: middle;
 			}
 			.chip:not([has-no-image]) {
@@ -65,6 +67,10 @@ class PaperChips extends GestureEventListeners(PolymerElement) {
 			.chip[selectable]:hover .delete {
 				color: black;
 			}
+			
+			iron-icon {
+			    margin-bottom: 3px;
+			}
 
 			[hidden] {
 				display: none;
@@ -74,137 +80,143 @@ class PaperChips extends GestureEventListeners(PolymerElement) {
 		<template is="dom-repeat" items="[[items]]">
 			<paper-chip
 				class="chip"
-				selectable$="[[!item.fixed]]"
-				has-no-image$="[[!item.image]]">
-				<img src="[[_defaultToEmpty(item.image)]]" hidden$="[[!item.image]]" />
-				[[item.name]]
+				selectable
+				index="[[index]]">
+				<img src="[[_getImage(item)]]" hidden$="[[!_hasImage(item)]]" />
+				[[_getName(item)]]
 				<iron-icon
 					icon="paper-chip:[[_getIcon(item)]]"
 					class="delete"
-					on-tap="_delete"
-				></iron-icon>
+					on-tap="_delete">	
+                </iron-icon>
 			</paper-chip>
 		</template>`;
-	}
+    }
 
-	static get is() {
-		return 'paper-chips';
-	}
+    static get is() {
+        return 'paper-chips';
+    }
 
-	static get properties() {
-		return {
+    static get properties() {
+        return {
 
-			/**
-			* True if no items are currently known
-			*/
-			empty: {
-				computed: '_computeEmpty(items.length)',
-				notify: true,
-				type: Boolean,
-			},
+            /**
+             * Array of chips
+             *
+             * ```js
+             * [
+             *  { id: 'apples', name: 'Apples', image:'apple.jpg' },
+             *  { id: 'pears', name: 'Pears', image:'pear.jpg'},
+             *  { id: 'bananas', name: 'Bananas', fixed: true}
+             * ]
+             * ```
+             */
+            items: {
+                notify: true,
+                type: Array,
+                value: [],
+                observer: '_changeItems'
+            },
+        };
+    }
 
-			/**
-			* Array of chips
-			*
-			* ```js
-			* [
-			*  { id: 'apples', name: 'Apples', image:'apple.jpg' },
-			*  { id: 'pears', name: 'Pears', image:'pear.jpg'},
-			*  { id: 'bananas', name: 'Bananas', fixed: true}
-			* ]
-			* ```
-			*/
-			items: {
-				notify: true,
-				type: Array,
-				value: [],
-			},
-		};
-	}
+    /**
+     * @param item
+     * @private
+     */
+    _hasImage(item) {
+        return this._getImage(item) === '' ? false : true;
+    }
 
-	/**
-	* Adds a chip
-	* @param {object} item To be added chip
-	* @returns {void}
-	*/
-	add(item) {
-		// Only chips with a visible name
-		if (typeof item.name === 'undefined' || item.name === '') {
-			return;
-		}
+    /**
+     * @param newValue
+     * @private
+     */
+    _changeItems(newValue) {
+        if (!Array.isArray(newValue)) {
+            this.items = [];
+        }
+    }
 
-		// Needs to use Polymer push to trigger data binding
-		this.push('items', item);
-	}
+    /**
+     * @param item
+     * @private
+     */
+    _getImage(item) {
+        return (item !== null && typeof item === 'object' && item.image) ? item.image : '';
+    }
 
-	/**
-	* Removes a chip
-	*
-	* Note that this will also remove chips marked as 'fixed'.
-	* @param {number} itemIndex Index of the to be removed chip
-	* @returns {void}
-	*/
-	remove(itemIndex) {
-		const removedItemId = this.items[itemIndex].id;
+    /**
+     * @param item
+     * @private
+     */
+    _getIcon(item) {
+        return (item !== null && typeof item === 'object' && item.fixed) ? 'lock' : 'cancel';
+    }
 
-		// Needs to use Polymer splice to trigger data binding
-		this.splice('items', itemIndex, 1);
+    /**
+     * @param item
+     * @return {*}
+     * @private
+     */
+    _getName(item) {
+        return (item !== null && typeof item === 'object' && item.name) ?  item.name :  item;
+    }
 
-		this.dispatchEvent(new CustomEvent('delete-item', {
-			bubbles: true,
-			composed: true,
-			detail: {
-				itemId: removedItemId,
-			},
-		}));
-	}
+    /**
+     * Adds a chip
+     * @param {object} item To be added chip
+     * @returns {void}
+     */
+    add(item) {
 
-	/**
-	* Removes the last chip
-	*
-	* Note that this will also remove chips marked as 'fixed'.
-	* @returns {void}
-	*/
-	removeLast() {
-		// Ignore if there are no chips left
-		if (this.items.length === 0) {
-			return;
-		}
+        // Needs to use Polymer push to trigger data binding
+        this.push('items', item);
+    }
 
-		this.remove(this.items.length - 1);
-	}
+    /**
+     * Removes a chip
+     *
+     * Note that this will also remove chips marked as 'fixed'.
+     * @param {number} itemIndex Index of the to be removed chip
+     * @returns {void}
+     */
+    remove(itemIndex) {
 
-	/**
-	* Handles clicks on the delete icon
-	* @param {any} e Event for deletion
-	* @returns {void}
-	*/
-	_delete(e) {
-		if (e.model.item.fixed) {
-			// Cannot delete fixed items.
-			return;
-		}
+        // Needs to use Polymer splice to trigger data binding
+        let item = this.splice('items', itemIndex, 1);
 
-		const itemIndex = this.items.indexOf(e.model.item);
-		if (itemIndex > -1) {
-			this.remove(itemIndex);
-		}
-	}
+        this.dispatchEvent(new CustomEvent('delete-item', {
+            bubbles: true,
+            composed: true,
+            detail: {
+                item
+            },
+        }));
+    }
 
-	_computeEmpty(nrItems) {
-		return nrItems === 0;
-	}
+    /**
+     * Removes the last chip
+     *
+     * Note that this will also remove chips marked as 'fixed'.
+     * @returns {void}
+     */
+    removeLast() {
+        // Ignore if there are no chips left
+        if (this.items.length === 0) {
+            return;
+        }
 
-	/**
-	 * @param {string} str String value
-	 * @returns {string} Provided value or empty string
-	 */
-	_defaultToEmpty(str) {
-		return str || '';
-	}
+        this.remove(this.items.length - 1);
+    }
 
-	_getIcon(item) {
-		return item.fixed ? 'lock' : 'cancel';
-	}
+    /**
+     * Handles clicks on the delete icon
+     * @param {any} e Event for deletion
+     * @returns {void}
+     */
+    _delete(e) {
+        this.remove(e.target.parentElement.index)
+    }
 }
 window.customElements.define(PaperChips.is, PaperChips);
